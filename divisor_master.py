cd307aaace80e3743de4a33ac96eadce8a988bb8
SERJ <serj.shepelevich@gmail.com>
2020-06-24 00:59:23 +0300

    5 урок + исправления

--- divisor_master.py
+++ divisor_master.py
@@ -18,15 +18,9 @@ def list_dividers(n):
     return list_d
 #3) выводит самый большой простой делитель числа.
 def largest_prime_div(n):
-    if IsPrime(n) == True:
-        return n
-    else:
-        list_divs = list_dividers(n)
-        list_divs.reverse()
-        for i in list_divs:
-            if IsPrime(i) == True:
-                return i
-        return None
+    list_d = map(lambda x:x if IsPrime(x) else 0,list_dividers(n))
+    return max(list_d)
+
 #4) функция выводит каноническое разложение числа на простые множители;
 def canonical_decomposition(n):
     temp_number = n
@@ -42,7 +36,15 @@ def canonical_decomposition(n):
                 d += 1
         else:
             d += 1
-    return list_divs
+    return to_string(list_divs, n)
+
+def to_string(list_divs, n):
+    string_dic = str(n) + ' = '
+    temp = ''
+    for i in set(list_divs):
+        temp += " * " + str(i) + '**' + str(list_divs.count(i))
+    return string_dic + temp[2:]
+
 
 #5)функция выводит самый большой делитель (не обязательно простой) числа.
 def largest_div(n):
@@ -54,7 +56,7 @@ def largest_div(n):
 
 
 
-n = 609840
+n = 144
 print("Число является простым " , IsPrime(n))
 print("список всех делителей числа: ", list_dividers(n))
 print("самый большой простой делитель числа: ", largest_prime_div(n))
